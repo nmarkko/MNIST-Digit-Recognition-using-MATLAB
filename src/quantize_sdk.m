65616dca06e1e61751560b6c4a4c6f4c3571dcc5
nmarkko <50952339+nmarkko@users.noreply.github.com>
2020-09-17 12:38:50 +0200

    Update quantize_sdk.m

--- src/quantize_sdk.m
+++ src/quantize_sdk.m
@@ -61,13 +61,21 @@ fprintf(fileIDb,'u32 bias_array[2][30]={ {');
 for i=1:30
     fprintf(fileIDb,'0x');
     fprintf(fileIDb,num2hex(q,b{1,1}(i,1)));
-    fprintf(fileIDb,', \n');
+     if (i== 30)
+    else
+        fprintf(fileIDb,', ');
+    end
+    fprintf(fileIDb,'\n');
 end
 fprintf(fileIDb,'}, {');
 for i=1:10
     fprintf(fileIDb,'0x');
     fprintf(fileIDb,num2hex(q,b{1,2}(i,1)));
-    fprintf(fileIDb,'\n');
+    if (i== 10)
+    else
+        fprintf(fileIDb,', ');
+    end
+    fprintf(fileIDb,', \n');
 end
 fprintf(fileIDb,'}};');
 fclose(fileIDb);
